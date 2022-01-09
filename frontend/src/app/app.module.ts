7d9da5695cc5218a3c7b8c46ff2c28622ebeaa05
arek <arkadiusz.pomaranski@embiq.com>
2022-01-09 16:14:03 +0100

    Add convarter, Add m to cm and cm to m units

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputNumberModule,
+    FormsModule,
+    DropdownModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ frontend/src/app/models/app.model.ts
@@ -0,0 +1,6 @@
+export interface UnitInterface {
+  label: string;
+  multiply: boolean;
+  figureForCalculation: number;
+  resultUnit: string;
+}
